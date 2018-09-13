68af5030a01cd547970b79122e846c159d76baf2
MrCocoaCat <liyubo_liyubo@163.com>
2018-09-13 13:34:14 +0800

    modify execept

--- LearnPython/except.py
+++ LearnPython/except.py
@@ -1,8 +1,28 @@
-try :
-    num = int(input("please input your number： "))
+try:
+    num = 0
     ret = 100/num
     print("结果为{}".format(ret))
 
 except ZeroDivisionError as e:
     print(e)
+
+
+#自定义一个异常,继承ValueError
+
+class DanaError(ValueError):
+    pass
+
+
+
+try:
+    print("test")
+    # 手动引发异常
+    raise DanaError
+except NameError as e:
+    print("NameError")
+except ValueError as e:
+    print("ValueError")
+finally:
+    print("最后执行")
+
 
