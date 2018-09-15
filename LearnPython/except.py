2ac360ae833a943790f61d1309fd8d72bc22ef5d
MrCocoaCat <liyubo_liyubo@163.com>
2018-09-15 19:09:22 +0800

    modify execept2

--- LearnPython/except.py
+++ LearnPython/except.py
@@ -1,3 +1,4 @@
+
 try:
     num = 0
     ret = 100/num
@@ -13,7 +14,6 @@ class DanaError(ValueError):
     pass
 
 
-
 try:
     print("test")
     # 手动引发异常
@@ -25,4 +25,45 @@ except ValueError as e:
 finally:
     print("最后执行")
 
+# 自定义异常，方便扩展
 
+
+# 日历模块
+import calendar
+cal = calendar.calendar(2017)
+print(cal)
+
+# time 模块 1970年
+# UTC时间，国际标准时间
+import time
+
+# 与标准时间的时间差
+print(time.timezone)
+
+# 获取时间戳
+print(time.time())
+
+# sleep 1 秒
+time.sleep(1)
+
+
+import datetime
+
+print(datetime.date(2018, 3,26))
+
+import os
+
+# 获取当前文件目录
+mydir = os.getcwd()
+print(mydir)
+
+# chdir()改变当前工作目录
+ld = os.listdir()
+print(ld)
+
+# 递归创建文件夹
+#rst = os.makedirs("test")
+
+
+# 执行系统命令
+rst = os.system("ls")
