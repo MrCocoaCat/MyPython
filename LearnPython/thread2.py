5465571b9738999b352147c0e76a50ea680d57c5
MrCocoaCat <liyubo_liyubo@163.com>
2018-10-12 09:42:12 +0800

    Update LearnPython/thread2.py

--- LearnPython/thread2.py
+++ LearnPython/thread2.py
@@ -3,21 +3,22 @@ import _thread as thread
 
 def loop1(in1):
     print("start loop1 at ",time.ctime())
-    print("参数",in1)
+    print("par ",in1)
     time.sleep(4)
     print("end loop1 at ",time.ctime())
 
 
-def loop2():
+def loop2(in1,in2):
     print("start loop2",time.ctime())
+    print("par 1", in1,"par 2", in2 )
     time.sleep(2)
     print("end loop2",time.ctime())
 
 
 def main():
     print("start at",time.ctime())
-    thread.start_new_thread(loop1, ())  
-    thread.start_new_thread(loop2, ())
+    thread.start_new_thread(loop1, ("a",))
+    thread.start_new_thread(loop2, ("b", "c"))
 
 
     print("all enf",time.ctime())
