15807f92311d75356eb4fcc0afb8685d31f11d55
MrCocoaCat <liyubo_liyubo@163.com>
2018-10-18 09:30:51 +0800

    modify thread

--- LearnPython/16_thread5.py
+++ LearnPython/16_thread5.py
@@ -18,3 +18,60 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+class RadosThread(threading.Thread):
+    def __init__(self, target, args=None):
+        self.args = args
+        self.target = target
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.retval = self.target(*self.args)
+
+# time in seconds between each call to t.join() for child thread
+POLL_TIME_INCR = 0.5
+
+def run_in_thread(target, args, timeout=0):
+    interrupt = False
+
+    countdown = timeout
+    t = RadosThread(target, args)
+
+    # allow the main thread to exit (presumably, avoid a join() on this
+    # subthread) before this thread terminates.  This allows SIGINT
+    # exit of a blocked call.  See below.
+    t.daemon = True
+
+    t.start()
+    try:
+        # poll for thread exit
+        while t.is_alive():
+            t.join(POLL_TIME_INCR)
+            if timeout and t.is_alive():
+                countdown = countdown - POLL_TIME_INCR
+                if countdown <= 0:
+                    raise KeyboardInterrupt
+
+        t.join()        # in case t exits before reaching the join() above
+    except KeyboardInterrupt:
+        # ..but allow SIGINT to terminate the waiting.  Note: this
+        # relies on the Linux kernel behavior of delivering the signal
+        # to the main thread in preference to any subthread (all that's
+        # strictly guaranteed is that *some* thread that has the signal
+        # unblocked will receive it).  But there doesn't seem to be
+        # any interface to create t with SIGINT blocked.
+        interrupt = True
+
+    if interrupt:
+        t.retval = -errno.EINTR
+    return t.retval
---
+++ LearnPython/22_mulprocess.py
@@ -0,0 +1,23 @@
+import multiprocessing
+from time import sleep,ctime
+import os
+
+class ClockProcess(multiprocessing.Process):
+
+    def __init__(self,interval):
+        super().__init__()
+        self.interval = interval
+
+    def run(self):
+         while True:
+            print("The time is %s" % ctime())
+            print(os.getpid())
+            print(os.getppid())
+            sleep(self.interval)
+
+if __name__=='__main__':
+    p= ClockProcess(3)
+    print(os.getpid())
+    p.start()
+    p.join()
+
