eabc32ca513314c40ebe251fd6228193614e1615
MrCocoaCat <liyubo_liyubo@163.com>
2018-10-25 14:18:30 +0800

    add 可变参数.py

---
+++ LearnPython/可变参数.py
@@ -0,0 +1,14 @@
+#*args表示任何多个无名参数，它是一个tuple；
+#**kwargs表示关键字参数，它是一个 dict。
+#并且同时使用*args和**kwargs时，*args参数列必须要在**kwargs前，
+#像foo(a=1, b='2', c=3, a', 1, None, )这样调用的话，会提示语法错误
+def foo(*args, **kwargs):
+    print('args =', args)
+    print ('kwargs = ', kwargs)
+    print ('-----------------------')
+
+if __name__ == '__main__':
+    foo(1, 2, 3, 4)
+    foo(a=1, b=2, c=3)
+    foo(1, 2, 3, 4, a=1, b=2, c=3)
+    foo('a', 1, None, a=1, b='2', c=3)
