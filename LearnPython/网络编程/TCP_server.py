389eba93edf6d0f96b5ccac71a32aa91ebc6f33f
MrCocoaCat <liyubo_liyubo@163.com>
2018-10-29 09:36:31 +0800

    finish  TCP_server

--- LearnPython/网络编程/TCP_server.py
+++ LearnPython/网络编程/TCP_server.py
@@ -5,20 +5,29 @@ def tco_srv():
     # SOCK_STREAM表示其为TCP 模式
     sock =  socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-    addr = ("127.0.0.1", 8998)
+    adr = ("127.0.0.1", 8998)
     # 2. 绑定socket
-    sock.bind(addr)
+    sock.bind(adr)
 
     # 3. 监听访问的socket
     sock.listen()
 
     while True:
+
         # 4.接收访问的socket,建立通信链路
-        skt,addr = sock.accept()
+        skt,adr = sock.accept()
+
         # 5, 接收对方发送的内容，500 表示buffer
         msg = skt.recv(500)
         msg =msg.decode()
 
         print(msg)
 
-        rst =
+        rst = "this is server，recirve from {0}".format(adr)
+        # 6. 反馈消息
+        skt.send(rst.encode())
+        # 关闭连接通路
+        skt.close()
+
+if __name__ == '__main__':
+    tco_srv()
