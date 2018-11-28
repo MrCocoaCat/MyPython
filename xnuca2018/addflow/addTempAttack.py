68f8b27ba9735fa5de6c27b1a62a7f6bb14f9a8e
MrCocoaCat <liyubo_liyubo@163.com>
2018-11-28 17:43:40 +0800

    modify Attack

--- xnuca2018/addflow/addTempAttack.py
+++ xnuca2018/addflow/addTempAttack.py
@@ -27,48 +27,52 @@ def ssh(sys_ip,cmds):
     ssh.close()
     return result
 
-def delcomm(hostip):
+def delcomm(hostip,attckip):
     """
     :param hostip:执行命令的服务器IP
     :return:
     """
     print hostip
     dest="10.121.100.0/24"
-    for i in range(20):
-        src="10.121.21"+str(i)
-        delofcmd="ip,nw_src="+src+",nw_dst="+dest
-        cmds ="ovs-ofctl del-flow br_unicom "+"\""+delofcmd+"\""
-        print cmds
+    src=attckip
+    delofcmd="ip,nw_src="+src+",nw_dst="+dest
+    cmds ="ovs-ofctl del-flow br_unicom "+"\""+delofcmd+"\""
+    print cmds
         #result=ssh(hostip,cmds)
         #print result
 
-def addcomm(hostip):
+def addcomm(hostip,attckip):
     """
     :param hostip:执行命令的服务器IP
     :return:
     """
     print hostip
     dest="10.121.100.0/24"
-    for i in range(20):
-        src="10.121.21"+str(i)
-        addofcmd="ip,nw_src="+src+",nw_dst="+dest+",priority=2,action=drop"
-        cmds ="ovs-ofctl add-flow br_unicom "+"\""+addofcmd+"\""
-        print cmds
+    src=attckip
+    addofcmd="ip,nw_src="+src+",nw_dst="+dest+",priority=2,action=drop"
+    cmds ="ovs-ofctl add-flow br_unicom "+"\""+addofcmd+"\""
+    print cmds
         #result=ssh(hostip,cmds)
         #print result
 
-def main():
-    if sys.argv[1] == 1 :
+def main(argv):
+    print argv
+    print argv==1
+    if argv == "1":
+        print "add"
         for i in range(1,21):
-                ip="192.168.0."+str(100+i)
-                addcomm(hostip=ip)
+                hostip="192.168.0."+str(100+i)
+                attckip="10.121.21."+str(i)
+                addcomm(hostip=hostip,attckip=attckip)
                 print "*" * 20
-    elif sys.argv[1] == 0:
+    elif argv == "0":
+        print "del"
         for i in range(1,21):
                 ip="192.168.0."+str(100+i)
-                delcomm(hostip=ip)
+                attckip = "10.121.21." + str(i)
+                delcomm(hostip=ip,attckip=attckip)
                 print "*" * 20
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
