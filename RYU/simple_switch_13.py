f02503bbd0beb5b5b328013b7f6175c73982c094
MrCocoaCat <liyubo_liyubo@163.com>
2018-12-24 16:32:51 +0800

    modify simple_switch

--- RYU/simple_switch_13.py
+++ RYU/simple_switch_13.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,54 +25,50 @@ from ryu.lib.packet import ether_types
 
 # 继承ryu.base.app_manager.RyuApp
 class SimpleSwitch13(app_manager.RyuApp):
-	# 指定openflow版本
+    # 指定openflow版本
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-	
 
-	def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-		ofproto = datapath.ofproto
-		parser = datapath.ofproto_parser
-		
-		# Instruction 规定对符合match时，所进行的指令
-		# OFPInstructionGotoTable
-		# OFPInstructionWriteMetadata:寫入Metadata 以做為下一個table 所需的參考資料。
-		# OFPInstructionActions:在目前的action set中寫入新的action，如果有相同的则覆盖
-		# OFPInstructionMeter:指定該封包到所定義的meter table。
-		inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-											 actions)]
-		# APPLY_ACTIONS为立即为报文执行指令
-		
-		if buffer_id:
-			# 消息类别为OFPFlowMod，instructions为指令
-			mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-									priority=priority, match=match,
-									instructions=inst)
-		else:
-			mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-									match=match, instructions=inst)
-									
-		# 发送至switch
-		datapath.send_msg(mod)
-		
-		
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Instruction 规定对符合match时，所进行的指令
+        # OFPInstructionGotoTable
+        # OFPInstructionWriteMetadata:寫入Metadata 以做為下一個table 所需的參考資料。
+        # OFPInstructionActions:在目前的action set中寫入新的action，如果有相同的则覆盖
+        # OFPInstructionMeter:指定該封包到所定義的meter table。
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                                 actions)]
+        # APPLY_ACTIONS为立即为报文执行指令
+        if buffer_id:
+            # 消息类别为OFPFlowMod，instructions为指令
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                        priority=priority, match=match,
+                                        instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                        match=match, instructions=inst)
+            # 发送至switch
+        datapath.send_msg(mod)
+
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-		# 事件管理
-		# 事件物件（Event Object ）做為參數
-		# ryu.controller.handler.set_ev_cls 最为其修飾函數
-		# ryu.controller.ofp_event.EventOFP<消息名称>
-		# ofp_event.EventOFP<SwitchFeatures>即消息类型为SwitchFeatures
-		# CONFIG_DISPATCHER状态：接收SwitchFeatures 訊息
-		# ev.mag 存储消息类别实例，ryu.ofproto.ofproto_v1_3_parser.OFPSwitchFeatures
-		# datapath 类存储讯息相关事件 ryu.controller.controller.Datapath
+        # 事件管理
+        # 事件物件（Event Object ）做為參數
+        # ryu.controller.handler.set_ev_cls 最为其修飾函數
+        # ryu.controller.ofp_event.EventOFP<消息名称>
+        # ofp_event.EventOFP<SwitchFeatures>即消息类型为SwitchFeatures
+        # CONFIG_DISPATCHER状态：接收SwitchFeatures 訊息
+        # ev.mag 存储消息类别实例，ryu.ofproto.ofproto_v1_3_parser.OFPSwitchFeatures
+        # datapath 类存储讯息相关事件 ryu.controller.controller.Datapath
         datapath = ev.msg.datapath
-		print datapath
+        print datapath
         ofproto = datapath.ofproto
-		print ofproto
+        print ofproto
         parser = datapath.ofproto_parser
 
         # install table-miss flow entry
@@ -81,31 +78,28 @@ class SimpleSwitch13(app_manager.RyuApp):
         # 128, OVS will send Packet-In with invalid buffer_id and
         # truncated packet data. In that case, we cannot output packets
         # correctly.  The bug has been fixed in OVS v2.1.0.
-		
-		# 封包
-		#當封包沒有match 任何一個普通Flow Entry 時，則觸發Packet-In。
+        # 封包
+        #當封包沒有match 任何一個普通Flow Entry 時，則觸發Packet-In。
         match = parser.OFPMatch()
-		
-		# OFPActionOutput类是用来指定封包的类
-		# 指定为OUTPUT action 类别
-		# OFPP_CONTROLLER:轉送到Controller 的Packet-In 訊息
-		# OFPP_FLOOD:轉送（Flood）到所有VLAN 的物理連接埠，除了來源埠跟已閉鎖的埠
-		#
+        # OFPActionOutput类是用来指定封包的类
+        # 指定为OUTPUT action 类别
+        # OFPP_CONTROLLER:轉送到Controller 的Packet-In 訊息
+        # OFPP_FLOOD:轉送（Flood）到所有VLAN 的物理連接埠，除了來源埠跟已閉鎖的埠
+
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
-										  # 当指定OFPCML_NO_BUFFER时，将全部加入packet-in，
-										  #不会暂存在交换机中
-		# 优先级为0
+                                          # 当指定OFPCML_NO_BUFFER时，将全部加入packet-in，
+                                          #不会暂存在交换机中
+        # 优先级为0
         self.add_flow(datapath, 0, match, actions)
-	
-	
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         # If you hit this you might want to increase
         # the "miss_send_length" of your switch
-		# 未 match 任何一个FLOW Entry 时，触发Packet-in
-		# PacketIn事件，接收位置目的的封包
-		# MAIN_DISPATCHER：一般状态
+        # 未 match 任何一个FLOW Entry 时，触发Packet-in
+        # PacketIn事件，接收位置目的的封包
+        # MAIN_DISPATCHER：一般状态
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
@@ -113,17 +107,17 @@ class SimpleSwitch13(app_manager.RyuApp):
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-		
+
         in_port = msg.match['in_port']
-		
-		# 获取源地址，目的地址
+
+        # 获取源地址，目的地址
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
-		# 	
+
         dst = eth.dst
         src = eth.src
 
@@ -155,12 +149,10 @@ class SimpleSwitch13(app_manager.RyuApp):
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
-		
-		# 
+
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
-		# 发送至switch
+        # 发送至switch
         datapath.send_msg(out)
-	
-	
+
 
