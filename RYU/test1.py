461baabb8278aeda21e9d707a3ad1329bd5da14b
MrCocoaCat <liyubo_liyubo@163.com>
2018-12-28 19:26:19 +0800

    modify test1.py

--- RYU/test1.py
+++ RYU/test1.py
@@ -27,27 +27,47 @@ class SimpleSwitch13(app_manager.RyuApp):
         print ofproto
         parser = datapath.ofproto_parser
 
-        match = parser.OFPMatch(in_port=12, eth_dst='0025-9095-dcff')
-        actions = [parser.OFPActionOutput(11)]
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        match1 = parser.OFPMatch(in_port=12, eth_dst='0025-9095-dcff')
+        actions1 = [parser.OFPActionOutput(11)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions1)]
 
-        mod = parser.OFPFlowMod(datapath,
-                                cookie=0,
-                                cookie_mask=0,
-                                table_id=0,
-                                #command=ofproto.OFPFC_ADD,
-                                command=ofproto.OFPFC_DELETE,
-                                idle_timeout=0,
-                                hard_timeout=0,
-                                priority=1,
-                                buffer_id=ofproto.OFP_NO_BUFFER,
-                                out_port=ofproto.OFPP_ANY,
-                                out_group=ofproto.OFPG_ANY,
-                                flags=0,
-                                match=match,
-                                instructions=inst)
-        print mod
-        req = datapath.send_msg(mod)
+        mod1 = parser.OFPFlowMod(datapath,
+                                 cookie=0,
+                                 cookie_mask=0,
+                                 table_id=0,
+                                 command=ofproto.OFPFC_DELETE,
+                                 idle_timeout=0,
+                                 hard_timeout=0,
+                                 priority=1,
+                                 buffer_id=ofproto.OFP_NO_BUFFER,
+                                 out_port=ofproto.OFPP_ANY,
+                                 out_group=ofproto.OFPG_ANY,
+                                 flags=0,
+                                 match=match1,
+                                 instructions=inst)
+        print mod1
+        req = datapath.send_msg(mod1)
         print req
 
+        match2 = parser.OFPMatch(in_port=11, eth_dst='0025-9095-dcfb')
+        actions2 = [parser.OFPActionOutput(12)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions2)]
+
+        mod2 = parser.OFPFlowMod(datapath,
+                                 cookie=0,
+                                 cookie_mask=0,
+                                 table_id=0,
+                                 command=ofproto.OFPFC_DELETE,
+                                 idle_timeout=0,
+                                 hard_timeout=0,
+                                 priority=1,
+                                 buffer_id=ofproto.OFP_NO_BUFFER,
+                                 out_port=ofproto.OFPP_ANY,
+                                 out_group=ofproto.OFPG_ANY,
+                                 flags=0,
+                                 match=match2,
+                                 instructions=inst)
+        print mod2
+        req = datapath.send_msg(mod2)
+        print req
 
--- RYU/test2-swith.py
+++ RYU/test2-swith.py
@@ -11,12 +11,12 @@ import ryu.app.ofctl.api
 
 # 继承ryu.base.app_manager.RyuApp
 class SimpleSwitch13(app_manager.RyuApp):
-    # 指定openflow版本
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.num = 0
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
@@ -62,47 +62,33 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
+        self.num += 1
+        print "round num: %d" % self.num
         datapath = ev.msg.datapath
-        print "datapath:"
-        print datapath
+        # print "datapath:"
+        # print datapath
         ofproto = datapath.ofproto
-        print "ofproto:"
-        print ofproto
+        # print "ofproto:"
+        # print ofproto
         parser = datapath.ofproto_parser
-
-        # install table-miss flow entry
-        # We specify NO BUFFER to max_len of the output action due to
-        # OVS bug. At this moment, if we specify a lesser number, e.g.,
-        # 128, OVS will send Packet-In with invalid buffer_id and
-        # truncated packet data. In that case, we cannot output packets
-        # correctly.  The bug has been fixed in OVS v2.1.0.
-        # 封包
-        # 當封包沒有match 任何一個普通Flow Entry 時，則觸發Packet-In。
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
-        # 优先级为0
         self.add_flow(datapath, 0, match, actions)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        # 未 match 任何一个FLOW Entry 时，触发Packet-in
-        # PacketIn事件，接收位置目的的封包
-        # MAIN_DISPATCHER：一般状态
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
+        self.num += 1
+        print "round num: %d" % self.num
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
-        print "msg is"
-        print msg
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
-        print "in_port"
-        print in_port
+        print "in_port:%d" % in_port
         # 获取源地址，目的地址
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
@@ -112,15 +98,11 @@ class SimpleSwitch13(app_manager.RyuApp):
         dst = eth.dst
         src = eth.src
         dpid = datapath.id
-        # mac_to_port为类内定义的字典
-        # 查找键值，若无想对应的键值，则设置
-        # 如果字典中包含有给定键，则返回该键对应的值，否则返回为该键设置的值。
         self.mac_to_port.setdefault(dpid, {})
-        self.logger.info("packet in %x %s %s %s", dpid, src, dst, in_port)
+        self.logger.info("packet in %x src:%s dst:%s in_port:%s", dpid, src, dst, in_port)
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
-        # 如果目的IP 在字典中，获取其值
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
