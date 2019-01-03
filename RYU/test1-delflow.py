51fa10d22e44822906a9d3f295690f08db64856c
MrCocoaCat <liyubo_liyubo@163.com>
2019-01-03 17:59:48 +0800

    modify test1

--- RYU/test1-delflow.py
+++ RYU/test1-delflow.py
@@ -11,10 +11,7 @@ from ryu.lib.packet import ether_types
 
 # 继承ryu.base.app_manager.RyuApp
 
-a = {9: "0025-9095-dcfb",
-     11: "0025-9095-dcfb",
-     12: "0025-9095-dcff"
-     }
+
 class SimpleSwitch13(app_manager.RyuApp):
     # 指定openflow版本
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -22,57 +19,37 @@ class SimpleSwitch13(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.switchDic = {"0x1741f4aa82eef": "192.168.125.47",
+                          "0x148bd3d3ad316": "192.168.125.43"}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
-
         datapath = ev.msg.datapath
-        print "datapath: %x " % datapath.id
-        ofproto = datapath.ofproto
-        print ofproto
-        parser = datapath.ofproto_parser
+        datapath_id = hex(datapath.id)
+        print datapath_id
+        if "192.168.125.47" != self.switchDic[datapath_id]:
+            ofproto = datapath.ofproto
+            parser = datapath.ofproto_parser
 
-        match1 = parser.OFPMatch(in_port=9, eth_dst='0025-9095-dcfb')
-        actions1 = [parser.OFPActionOutput(12)]
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions1)]
+            match1 = parser.OFPMatch(in_port=36)
+            actions1 = [parser.OFPActionOutput(27)]
+            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions1)]
 
-        mod1 = parser.OFPFlowMod(datapath,
-                                 cookie=0,
-                                 cookie_mask=0,
-                                 table_id=0,
-                                 command=ofproto.OFPFC_DELETE,
-                                 idle_timeout=0,
-                                 hard_timeout=0,
-                                 priority=1,
-                                 buffer_id=ofproto.OFP_NO_BUFFER,
-                                 out_port=ofproto.OFPP_ANY,
-                                 out_group=ofproto.OFPG_ANY,
-                                 flags=0,
-                                 match=match1,
-                                 instructions=inst)
-        print mod1
-        req = datapath.send_msg(mod1)
-        print req
-
-        match2 = parser.OFPMatch(in_port=12, eth_dst='0025-9095-dcff')
-        actions2 = [parser.OFPActionOutput(11)]
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions2)]
-
-        mod2 = parser.OFPFlowMod(datapath,
-                                 cookie=0,
-                                 cookie_mask=0,
-                                 table_id=0,
-                                 command=ofproto.OFPFC_DELETE,
-                                 idle_timeout=0,
-                                 hard_timeout=0,
-                                 priority=1,
-                                 buffer_id=ofproto.OFP_NO_BUFFER,
-                                 out_port=ofproto.OFPP_ANY,
-                                 out_group=ofproto.OFPG_ANY,
-                                 flags=0,
-                                 match=match2,
-                                 instructions=inst)
-        print mod2
-        req = datapath.send_msg(mod2)
-        print req
+            mod1 = parser.OFPFlowMod(datapath,
+                                     cookie=0,
+                                     cookie_mask=0,
+                                     table_id=0,
+                                     command=ofproto.OFPFC_DELETE,
+                                     idle_timeout=0,
+                                     hard_timeout=0,
+                                     priority=1,
+                                     buffer_id=ofproto.OFP_NO_BUFFER,
+                                     out_port=ofproto.OFPP_ANY,
+                                     out_group=ofproto.OFPG_ANY,
+                                     flags=0,
+                                     match=match1,
+                                     instructions=inst)
+            print mod1
+            req = datapath.send_msg(mod1)
+            print req
 
