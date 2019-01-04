59e80826f668ba7ed0c75706e84c3db27317ce0f
MrCocoaCat <liyubo_liyubo@163.com>
2019-01-04 09:38:25 +0800

    modify test1

--- RYU/test1-delflow.py
+++ RYU/test1-delflow.py
@@ -9,8 +9,6 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
-# 继承ryu.base.app_manager.RyuApp
-
 
 class SimpleSwitch13(app_manager.RyuApp):
     # 指定openflow版本
@@ -22,34 +20,44 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.switchDic = {"0x1741f4aa82eef": "192.168.125.47",
                           "0x148bd3d3ad316": "192.168.125.43"}
 
+    def del_port(self, datapath, inport, outport):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        match = parser.OFPMatch(in_port=inport)
+        actions = [parser.OFPActionOutput(outport)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath,
+                                 cookie=0,
+                                 cookie_mask=0,
+                                 table_id=0,
+                                 command=ofproto.OFPFC_DELETE,
+                                 idle_timeout=0,
+                                 hard_timeout=0,
+                                 priority=1,
+                                 buffer_id=ofproto.OFP_NO_BUFFER,
+                                 out_port=ofproto.OFPP_ANY,
+                                 out_group=ofproto.OFPG_ANY,
+                                 flags=0,
+                                 match=match,
+                                 instructions=inst)
+        print mod
+        req = datapath.send_msg(mod)
+        print req
+        pass
+
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         datapath_id = hex(datapath.id)
         print datapath_id
-        if "192.168.125.47" != self.switchDic[datapath_id]:
-            ofproto = datapath.ofproto
-            parser = datapath.ofproto_parser
+        if "192.168.125.47" == self.switchDic[datapath_id]:
+            self.del_port(datapath, inport=11, outport=8)
+            self.del_port(datapath, inport=8, outport=11)
 
-            match1 = parser.OFPMatch(in_port=36)
-            actions1 = [parser.OFPActionOutput(27)]
-            inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions1)]
+        elif "192.168.125.43" == self.switchDic[datapath_id]:
+            self.del_port(datapath, inport=36, outport=27)
+            self.del_port(datapath, inport=27, outport=38)
 
-            mod1 = parser.OFPFlowMod(datapath,
-                                     cookie=0,
-                                     cookie_mask=0,
-                                     table_id=0,
-                                     command=ofproto.OFPFC_DELETE,
-                                     idle_timeout=0,
-                                     hard_timeout=0,
-                                     priority=1,
-                                     buffer_id=ofproto.OFP_NO_BUFFER,
-                                     out_port=ofproto.OFPP_ANY,
-                                     out_group=ofproto.OFPG_ANY,
-                                     flags=0,
-                                     match=match1,
-                                     instructions=inst)
-            print mod1
-            req = datapath.send_msg(mod1)
-            print req
+
+
 
