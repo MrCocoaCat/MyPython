aa6c2b98c2b974e2bcfbb2ea9ccc6eb345523e4c
MrCocoaCat <liyubo_liyubo@163.com>
2019-01-09 11:28:29 +0800

    add eventlet

---
+++ LearnPython/eventlet/1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/9 10:13
+# @Author  : MrCocoaCat
+# @Email   : MrCocoaCat@aliyun.com
+# @File    : 1.py
+
+
+import eventlet
+import time
+
+def test(s):
+    print(s + "begin")
+    time.sleep(5)
+    print(s + "end")
+
+
+pool = eventlet.GreenPool(5)
+a = time.time()
+for i in range(5):
+    pool.spawn(test(str(i)))
+b = time.time()
+print b-a
