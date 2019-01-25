1a89ada41efa0b5667d612af415cc059c4aa5661
MrCocoaCat <liyubo_liyubo@163.com>
2019-01-25 12:46:24 +0800

    modifu simple_monitoe

--- RYU/simple_monitor_13.py
+++ RYU/simple_monitor_13.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
-
+# Copyright (C) 2016 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from operator import attrgetter
 
@@ -16,6 +29,7 @@ class SimpleMonitor13(simple_switch_13.SimpleSwitch13):
         super(SimpleMonitor13, self).__init__(*args, **kwargs)
         # 定义字典
         self.datapaths = {}
+        # 启动线程_monitor
         self.monitor_thread = hub.spawn(self._monitor)
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
@@ -33,6 +47,7 @@ class SimpleMonitor13(simple_switch_13.SimpleSwitch13):
                 self.logger.debug('unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
 
+    # 线程函数
     def _monitor(self):
         while True:
             for dp in self.datapaths.values():
