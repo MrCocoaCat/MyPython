5c846f153aa56e05659091c97580c2b1951ac1ae
MrCocoaCat <liyubo_liyubo@163.com>
2019-01-29 14:14:40 +0800

    add tcp.py

---
+++ LearnPython/socket网络编程/tcp.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/29 13:15
+# @Author  : MrCocoaCat
+# @Email   : MrCocoaCat@aliyun.com
+# @File    : tcp.py
+
+import argparse
+import socket
+
+def recvall(sock, length):
+    data = b''
+    while len(data) < length:
+        more = sock.recv(length - len(data))
+        if not more:
+            raise EOFError('was expecting %d bytes but only received'
+                           ' %d bytes before the socket closed'
+                           % (length, len(data)))
+        data += more
+    return data
+
+def server(interface, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((interface, port))
+    sock.listen(1)
+    print('Listening at', sock.getsockname())
+    while True:
+        print('Waiting to accept a new connection')
+        sc, sockname = sock.accept()
+        print('We have accepted a connection from', sockname)
+        print('  Socket name:', sc.getsockname())
+        print('  Socket peer:', sc.getpeername())
+        message = recvall(sc, 16)
+        print('  Incoming sixteen-octet message:', repr(message))
+        sc.sendall(b'Farewell, client')
+        sc.close()
+        print('  Reply sent, socket closed')
+
+def client(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    print('Client has been assigned socket name', sock.getsockname())
+    sock.sendall(b'Hi there, server')
+    reply = recvall(sock, 16)
+    print('The server said', repr(reply))
+    sock.close()
+
+
+if __name__ == '__main__':
+    choices = {'client': client, 'server': server}
+    # 创建对象
+    parser = argparse.ArgumentParser(description='Send and receive over TCP')
+    # 添加参数,choices 选项设置选择范围
+    parser.add_argument('role', choices=choices, help='which role to play')
+
+    parser.add_argument('host', help='interface the server listens at;'
+                        ' host the client sends to')
+    # type 指定参数类型
+    parser.add_argument('-p', metavar='PORT', type=int, default=1060,
+                        help='TCP port (default 1060)')
+    args = parser.parse_args()
+    #  根据
+    function = choices[args.role]
+    # 调用函数
+    function(args.host, args.p)
+
