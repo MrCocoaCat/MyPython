2fc52109a918ef6b4b2ce1f3e68152cd0ea2b6c3
liyubo <MrCocoaCat@aliyun.com>
2020-05-09 00:43:46 +0800

    dddd

--- LearnPython/1.py
+++ LearnPython/1.py
@@ -1,52 +1,65 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/20 10:53
+# @Author  : MrCocoaCat
+# @Email   : MrCocoaCat@aliyun.com
+# @File    : test.py
 from requests import put, get,delete,post
-from threading import Thread
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import queue
-
 import itertools
-import multiprocessing
-from time import sleep, ctime
-from multiprocessing import Process, Lock, Manager
 from concurrent.futures import ThreadPoolExecutor
 import threading
+import queue
+from urllib import request
+import gevent,time
 
-codebase_list = list(map(chr, range(ord('0'), ord('9') + 1))) + list(map(chr, range(ord('A'), ord('Z') + 1))) + list(map(chr, range(ord('a'), ord('z') + 1)))
+threadPool = ThreadPoolExecutor(max_workers=200)
+
+#codebase_list = list(map(chr, range(ord('0'), ord('9') + 1))) + list(map(chr, range(ord('A'), ord('Z') + 1))) + list(map(chr, range(ord('a'), ord('z') + 1)))
 codebase_list = list(map(chr, range(ord('a'), ord('z') + 1)))
 BASE_URL = 'http://192.168.255.111/practice/login/'
-flag = True
-# 参数times用来模拟网络请求的时间
+q = queue.Queue()
 
-def get_html(num):
-    global flag
-    a_list =[ ]
+def gener_passwd(num):
+    num = 8
+    a_list =[]
     for i in range(num):
         a_list.append(codebase_list)
         for p in itertools.product(*a_list):
-            # if flag:
             password = ''.join(p)
-            print(password)
-            data_json = {
-            'username': 'liyubo',
-            'password': password,
-            }
-            ret2 = post(BASE_URL, data=data_json)
-            print(ret2.json())
-            a = ret2.json()
-            if a['flag']:
-                print("is : %s" % password)
-                flag = False
+            q.put(password)
+
+
+def do_url(password):
+    data_json = {
+    'username': 'liyubo',
+    'password': password,
+    }
+    time.sleep(1)
+    print(data_json['password'])
+    return 0
+    ret2 = post(BASE_URL, data=data_json)
+    print(ret2.json())
+    a = ret2.json()
+    if a['flag']:
+        print("is : %s" % password)
+        flag = False
+
+
+def con():
+    while True:
+        a = q.get()
+        do_url(a)
 
 
 if __name__ == '__main__':
-    p_list = []
-    try:
-        for i in range(8):
-            thread1 = threading.Thread(target=get_html, args=(i,))
-            thread1.setDaemon(True)
-            thread1.start()
-            # p = multiprocessing.Process(target=get_html, args=(i,))
-            # p.daemon = True
-            # p.start()
-            #p_list.append(p)
-    except InterruptedError:
-        exit()
+    async_time_start = time.time()
+    threadPool.submit(gener_passwd, 4)
+    for i in range(0, 200):
+        future = threadPool.submit(con)
+
+    threadPool.shutdown(wait=True)
+
+
+    print("异步步cost", time.time() - async_time_start)
+
+
+
--- LearnPython/111.py
+++ LearnPython/111.py
@@ -1,8 +1,87 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2019/11/22 21:03
+# @Time    : 2019/8/20 10:53
 # @Author  : MrCocoaCat
 # @Email   : MrCocoaCat@aliyun.com
-# @File    : 111.py
-sdfsdfsdf = 12.2222222
-print(sdfsdfsdf)
-float()
+# @File    : test.py
+from requests import put, get,delete,post
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+import itertools
+import multiprocessing
+from time import sleep, ctime
+from multiprocessing import Process, Lock, Manager
+from concurrent.futures import ThreadPoolExecutor
+import threading
+import queue
+from urllib import request
+import gevent
+from gevent import Greenlet
+import gevent,time
+
+from gevent import monkey#打补丁（把下面有可能有IO操作的单独做上标记）
+monkey.patch_all()#打补丁
+
+#codebase_list = list(map(chr, range(ord('0'), ord('9') + 1))) + list(map(chr, range(ord('A'), ord('Z') + 1))) + list(map(chr, range(ord('a'), ord('z') + 1)))
+codebase_list = list(map(chr, range(ord('a'), ord('z') + 1)))
+BASE_URL = 'http://192.168.255.111/practice/login/'
+q = queue.Queue()
+
+def gener_passwd(num):
+    a_list =[]
+    for i in range(num):
+        a_list.append(codebase_list)
+        for p in itertools.product(*a_list):
+            password = ''.join(p)
+            q.put(password)
+
+
+def do_url(password):
+    data_json = {
+    'username': 'liyubo',
+    'password': password,
+    }
+    time.sleep(1)
+    print(data_json['password'])
+    return 0
+    ret2 = post(BASE_URL, data=data_json)
+    print(ret2.json())
+    a = ret2.json()
+    if a['flag']:
+        print("is : %s" % password)
+        flag = False
+
+
+def f(url):
+    print('GET:%s'%url)
+    resp = request.urlopen(url)
+    data = resp.read()
+    # file = open("data",'wb')#这里可以打开这两步，写入文件
+    # file.write(data)
+    print('%d bytes received from %s.'%(len(data),url))
+
+
+#异步模式w
+def consur():
+    l = []
+    while True:
+        passwd = q.get()
+        a = Greenlet.spawn(do_url, passwd)
+        l.append(a)
+    gevent.joinall(l)
+
+
+
+if __name__ == '__main__':
+    async_time_start = time.time()
+
+    p = gevent.spawn(gener_passwd,3)
+    c = gevent.spawn(consur)
+    gevent.joinall([p, c])
+
+
+    print("异步步cost", time.time() - async_time_start)
+
+
+
